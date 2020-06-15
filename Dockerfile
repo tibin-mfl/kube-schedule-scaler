cc240c3cadc9a9fb153d9983a622e1ba9c361ff4
Michele Sorcinelli <michelesr@users.noreply.github.com>
2020-06-15 14:55:46 +0100

    Refactor (#2)
    Changes:
    
    -  Use `python:3.8-alpine` base image
    - Remove unnecessary python packages and use `pip --no-cache-dir` to reduce image size 
    -  Remove support for reading config from S3 rather than annotations
    -  Remove references in readme and comments to StackSet (that is a Zalando custom resource not handled anymore by this fork)
    -  Restructure source code location to make it more readable
    - Replace cron daemon with internal scheduling implementation
    - Handle HPA update in a single API operation
    - Don't update objects that are already matching the desired number of replicas
    - The whole program now can be run as a single python command, rather than a startup script that spawns multiple processes
    - Removed missed jobs script (as not used anymore)
    - Use double quoted strings everywhere for consistency
    - Use logging methods everywhere for consistency (instead of print)
    - Add `LOG_LEVEL` environment variable to control logging level
    - Add security context to run kube-schedule-scaler pod as user 1000

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,16 @@
-From ubuntu:16.04
-MAINTAINER "eng.ahmed.elbakry@gmail.com"
+FROM python:3.8-alpine
 
-# Install python tools and dev packages
-RUN apt-get update \
-    && apt-get install -q -y --no-install-recommends  python3-pip python3-setuptools python3-wheel gcc \
-    && apt-get clean \ 
-    && rm -rf /var/lib/apt/lists/*
+RUN apk add --no-cache bash
+RUN pip install --no-cache-dir pykube croniter
 
-# set python 3 as the default python version
-RUN update-alternatives --install /usr/bin/python python /usr/bin/python3 1 \
-    && update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
-RUN pip3 install --upgrade pip requests setuptools pipenv
-RUN pip3 install pykube
-RUN pip3 install python-crontab
-RUN pip3 install croniter
-RUN pip3 install boto3
-RUN apt-get update &&  apt-get install -y apt-transport-https curl gnupg sudo \
-    && curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-RUN sudo touch /etc/apt/sources.list.d/kubernetes.list \
-    && echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-RUN sudo apt-get update \
-    && sudo apt-get install -y kubectl cron
+RUN adduser -u 1000 -D app && \
+    mkdir /app && \
+    chown app: /app
 
-ADD schedule_scaling /root/schedule_scaling
-COPY ./run_missed_jobs.py /root
-RUN chmod a+x /root/run_missed_jobs.py
-# allow us to override pykube resources
-COPY ./resources.py /root/schedule_scaling
-ENV PYTHONPATH "${PYTHONPATH}:/root/schedule_scaling"
+USER 1000
+WORKDIR /app
 
-COPY ./startup.sh /root
-RUN chmod a+x /root/startup.sh
-CMD /root/startup.sh
+COPY schedule_scaling/ /app/
+
+ENV PYTHONPATH "${PYTHONPATH}:/app/schedule_scaling"
+CMD ["python", "-u", "schedule_scaling.py"]
--- schedule_scaling/schedule_scaling.py
+++ schedule_scaling/schedule_scaling.py
@@ -1,37 +1,15 @@
-""" Collecting Deployments configured for Scaling """
-import os
-import pathlib
+#!/usr/bin/env python3
+import pykube
 import json
 import logging
-import shutil
-import pykube
-import re
-import urllib.request
-import boto3
-import resources
-from crontab import CronTab
+import os
+from resources import Deployment
+from datetime import datetime, timedelta, timezone
+from time import sleep
+from croniter import croniter
+from resources import Deployment
 
-EXECUTION_TIME = 'datetime.datetime.now().strftime("%d-%m-%Y %H:%M UTC")'
-crontab_instance = CronTab(user="root")
-
-def create_job_directory():
-    """ This directory will hold the temp python scripts to execute the scaling jobs """
-    temp__dir = '/tmp/scaling_jobs'
-    if os.path.isdir(temp__dir):
-        shutil.rmtree(temp__dir)
-    pathlib.Path(temp__dir).mkdir(parents=True, exist_ok=True)
-
-def clear_cron():
-    """ This is needed so that if any one removes his scaling action
-          it should not be trigger again """
-    crontab_instance.remove_all(comment="Scheduling_Jobs")
-
-def commit():
-    try:
-        crontab_instance.write()
-    except Exception as e:
-        print("An exception has been raised while trying to commit the crontab changes")
-        print(e)
+logging.getLogger().setLevel(os.environ.get('LOG_LEVEL', 'INFO'))
 
 def get_kube_api():
     """ Initiating the API from Service Account or when running locally from ~/.kube/config """
@@ -39,154 +17,146 @@ def get_kube_api():
         config = pykube.KubeConfig.from_service_account()
     except FileNotFoundError:
         # local testing
-        config = pykube.KubeConfig.from_file(os.path.expanduser('~/.kube/config'))
+        config = pykube.KubeConfig.from_file(os.path.expanduser("~/.kube/config"))
     api = pykube.HTTPClient(config)
     return api
 
+api = get_kube_api()
 
 def deployments_to_scale():
-    '''
+    """
     Getting the deployments configured for schedule scaling...
-    '''
-    api = get_kube_api()
+    """
     deployments = []
     scaling_dict = {}
     for namespace in list(pykube.Namespace.objects(api)):
         namespace = str(namespace)
-        for deployment in resources.Deployment.objects(api).filter(namespace=namespace):
-            annotations = deployment.metadata.get('annotations', {})
-            f_deployment = str(namespace + '/' + str(deployment))
+        for deployment in Deployment.objects(api).filter(namespace=namespace):
+            annotations = deployment.metadata.get("annotations", {})
+            f_deployment = str(namespace + "/" + str(deployment))
 
-            schedule_actions = parse_content(annotations.get('zalando.org/schedule-actions', None), f_deployment)
+            schedule_actions = parse_schedules(annotations.get("zalando.org/schedule-actions", "[]"), f_deployment)
 
             if schedule_actions is None or len(schedule_actions) == 0:
                 continue
 
-            deployments.append([deployment.metadata['name']])
+            deployments.append([deployment.metadata["name"]])
             scaling_dict[f_deployment] = schedule_actions
     if not deployments:
-        logging.info('No deployment is configured for schedule scaling')
+        logging.info("No deployment is configured for schedule scaling")
 
     return scaling_dict
 
-
-def deploy_job_creator():
-    """ Create CronJobs for configured Deployments """
-
-    deployments__to_scale = deployments_to_scale()
-    print("Deployments collected for scaling: ")
-    for deploy, schedules in deployments__to_scale.items():
-        deployment = deploy.split("/")[1]
-        namespace = deploy.split("/")[0]
-        for n in range(len(schedules)):
-            schedules_n = schedules[n]
-            replicas = schedules_n.get('replicas', None)
-            minReplicas = schedules_n.get('minReplicas', None)
-            maxReplicas = schedules_n.get('maxReplicas', None)
-            schedule = schedules_n.get('schedule', None)
-            print("Deployment: %s, Namespace: %s, Replicas: %s, MinReplicas: %s, MaxReplicas: %s, Schedule: %s"
-                  % (deployment, namespace, replicas, minReplicas, maxReplicas, schedule))
-
-            with open("/root/schedule_scaling/templates/deployment-script.py", 'r') as script:
-                script = script.read()
-            deployment_script = script % {
-                'namespace': namespace,
-                'name': deployment,
-                'replicas': replicas,
-                'minReplicas': minReplicas,
-                'maxReplicas': maxReplicas,
-                'time': EXECUTION_TIME,
-            }
-            i = 0
-            while os.path.exists("/tmp/scaling_jobs/%s-%s.py" % (deployment, i)):
-                i += 1
-            script_creator = open("/tmp/scaling_jobs/%s-%s.py" % (deployment, i), "w")
-            script_creator.write(deployment_script)
-            script_creator.close()
-            cmd = ['. /root/.profile ; /usr/bin/python', script_creator.name,
-                   '2>&1 | tee -a /tmp/scale_activities.log']
-            cmd = ' '.join(map(str, cmd))
-            job = crontab_instance.new(command=cmd)
-            try:
-                job.setall(schedule)
-                job.set_comment("Scheduling_Jobs")
-            except Exception:
-                print('Deployment: %s has syntax error in the schedule' % (deployment))
-                job.delete()
-
-def parse_content(content, identifier):
-    if content == None:
-        return []
-
-    if is_valid_s3_url(content):
-        schedules = fetch_schedule_actions_s3(content)
-
-        if schedules == None:
-            return []
-
-        return parse_schedules(schedules, identifier)
-
-    if is_valid_url(content):
-        schedules = fetch_schedule_actions_from_url(content)
-
-        if schedules == None:
-            return []
-
-        return parse_schedules(schedules, identifier)
-
-    return parse_schedules(content, identifier)
-
-def is_valid_url(url):
-    return re.search('^(https?)://(\\S+)\.(\\S{2,}?)(/\\S+)?$', url, re.I) != None
-
-def is_valid_s3_url(url):
-    return parse_s3_url(url) != None
-
-def parse_s3_url(url):
-    match = re.search('^s3://(\\S+?)/(\\S+)$', url, re.I)
-
-    if match == None:
-        return None
-
-    return {
-        'Bucket': match.group(1),
-        'Key': match.group(2)
-    }
-
-def fetch_schedule_actions_s3(url):
-    source = parse_s3_url(url)
-
-    print(source)
-
-    s3 = boto3.client('s3')
-    try:
-        element = s3.get_object(**source)
-    except:
-        print('Couldn\'t read %s' % (url))
-        return '[]'
-
-    return element['Body'].read().decode('utf-8')
-
-def fetch_schedule_actions_from_url(url):
-    request = urllib.request.urlopen(url)
-    try:
-        content = request.read().decode('utf-8')
-    except:
-        content = None
-    finally:
-        request.close()
-
-    return content
 
 def parse_schedules(schedules, identifier):
     try:
         return json.loads(schedules)
     except Exception as err:
-        print('%s - Error in parsing JSON %s with error' % (identifier, schedules), err)
+        logging.error("%s - Error in parsing JSON %s with error" % (identifier, schedules), err)
         return []
 
-if __name__ == '__main__':
-    create_job_directory()
-    clear_cron()
-    deploy_job_creator()
-    commit()
+
+def get_delta_sec(schedule):
+    # get current time
+    now = datetime.now()
+    # get the last previous occurrence of the cron expression
+    t = croniter(schedule, now).get_prev()
+    # convert now to unix timestamp
+    now = now.replace(tzinfo=timezone.utc).timestamp()
+    # return the delta
+    return now - t
+
+
+def process_deployment(deployment, schedules):
+    namespace, name = deployment.split("/")
+    for schedule in schedules:
+        # when provided, convert the values to int
+        replicas = schedule.get("replicas", None)
+        if replicas:
+            replicas = int(replicas)
+        min_replicas = schedule.get("minReplicas", None)
+        if min_replicas:
+            min_replicas = int(min_replicas)
+        max_replicas = schedule.get("maxReplicas", None)
+        if max_replicas:
+            max_replicas = int(max_replicas)
+
+        schedule_expr = schedule.get("schedule", None)
+        logging.debug("Deployment: %s, Namespace: %s, Replicas: %s, MinReplicas: %s, MaxReplicas: %s, Schedule: %s" % (name, namespace, replicas, min_replicas, max_replicas, schedule_expr))
+        # if less than 60 seconds have passed from the trigger
+        if get_delta_sec(schedule_expr) < 60:
+            # replicas might equal 0 so we check that is not None
+            if replicas != None:
+                scale_deployment(name, namespace, replicas)
+            # these can't be 0 by definition so checking for existence is enough
+            if min_replicas or max_replicas:
+                scale_hpa(name, namespace, min_replicas, max_replicas)
+
+
+def scale_deployment(name, namespace, replicas):
+    try:
+        deployment = Deployment.objects(api).filter(namespace=namespace).get(name=name)
+    except pykube.exceptions.ObjectDoesNotExist:
+        logging.warning("Deployment {}/{} does not exist".format(namespace, name))
+        return
+
+    if replicas == None or replicas == deployment.replicas:
+        return
+    deployment.replicas = replicas
+
+    time = datetime.now().strftime("%d-%m-%Y %H:%M UTC")
+    try:
+        deployment.update()
+        logging.info("Deployment {}/{} scaled to {} replicas at {}".format(namespace, name, replicas, time))
+    except Exception as e:
+        logging.error("Exception raised while updating deployment {}/{}".format(namespace, name))
+        logging.exception(e)
+
+
+def scale_hpa(name, namespace, min_replicas, max_replicas):
+    try:
+        hpa = pykube.HorizontalPodAutoscaler.objects(api).filter(namespace=namespace).get(name=name)
+    except pykube.exceptions.ObjectDoesNotExist:
+        logging.warning("HPA {}/{} does not exist".format(namespace, name))
+        return
+
+    # return if no values are provided
+    if not min_replicas and not max_replicas:
+        return
+
+    # return when both are provided but hpa is already up-to-date
+    if (hpa.obj["spec"]["minReplicas"] == min_replicas and
+        hpa.obj["spec"]["maxReplicas"] == max_replicas):
+        return
+
+    # return when only one of them is provided but hpa is already up-to-date
+    if ((not min_replicas and max_replicas == hpa.obj["spec"]["maxReplicas"]) or
+        (not max_replicas and min_replicas == hpa.obj["spec"]["minReplicas"])):
+        return
+
+    if min_replicas:
+        hpa.obj["spec"]["minReplicas"] = min_replicas
+
+    if max_replicas:
+        hpa.obj["spec"]["maxReplicas"] = max_replicas
+
+    time = datetime.now().strftime("%d-%m-%Y %H:%M UTC")
+    try:
+        hpa.update()
+        if min_replicas:
+            logging.info("HPA {}/{} minReplicas set to {} at {}".format(namespace, name, min_replicas, time))
+        if max_replicas:
+            logging.info("HPA {}/{} maxReplicas set to {} at {}".format(namespace, name, max_replicas, time))
+    except Exception as e:
+        logging.error("Exception raised while updating HPA {}/{}".format(namespace, name))
+        logging.exception(e)
+
+
+if __name__ == "__main__":
+    logging.info("Main loop started")
+    while True:
+        logging.debug("Getting deployments")
+        for deployment, schedules in deployments_to_scale().items():
+            process_deployment(deployment, schedules)
+        logging.debug("Waiting 50 seconds")
+        sleep(50)
